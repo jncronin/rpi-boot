fb1568e834d5f5d0e3cfcc47b9713dc5f110337a
jncronin <jncronin@0f3ef04d-8e58-4d2a-b558-b31704943fbb>
2013-02-09 09:37:58 +0000

    Added README and licence statements.
    git-svn-id: file:///var/johncronin-svn/rpi-boot@42 0f3ef04d-8e58-4d2a-b558-b31704943fbb

--- elf.c
+++ elf.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 2013 by John Cronin
+/* Copyright (C) 2013 by John Cronin <jncronin@tysos.org>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
